c02eedcd09fda0d74ce01d22bad9a93b52cb42a6
heweikang <heweikang@yafex.cn>
2024-04-10 18:25:27 +0800

    batch infer ok for rmbg14

--- sessions/rmbg14.py
+++ sessions/rmbg14.py
@@ -12,6 +12,10 @@ from transformers import AutoModelForImageSegmentation
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # device = torch.device('cpu')
 
+def batch_list(input_list, batch_size=16):
+    # Create batches of 16 samples each
+    return [input_list[i:i + batch_size] for i in range(0, len(input_list), batch_size)]
+
 class RMBG14Session(BaseSession):
     """This is a class representing a SiluetaSession object."""
     def __init__(
@@ -27,59 +31,104 @@ class RMBG14Session(BaseSession):
         self.providers = []
         self.model = AutoModelForImageSegmentation.from_pretrained("model_weights/rmbg14", trust_remote_code=True).eval().to(device)
     
+    # def predict(self, img: PILImage, *args, **kwargs) -> List[PILImage]:
+    #     """
+    #     Predict the mask of the input image.
+    #     Parameters:
+    #         img (PILImage): The input image to be processed.
+    #         *args: Variable length argument list.
+    #         **kwargs: Arbitrary keyword arguments.
+    #     Returns:
+    #         List[PILImage]: A list of post-processed masks.
+    #     """
+    #     normed_img = self.normalize(img, (0.5,0.5,0.5), (1.0, 1.0, 1.0), (768, 768)) # 可以上 1024x1024
+
+    #     with torch.no_grad():
+    #         masks_pt = self.model(torch.tensor(normed_img, device=device))
+        
+    #     masks = []
+    #     for i in range(2):
+    #         for j in range(2):
+    #             mask = masks_pt[i][j][0, 0].cpu().numpy()
+    #             ma, mi = np.max(mask), np.min(mask)
+    #             mask = (mask - mi) / (ma - mi)
+    #             mask = Image.fromarray((mask * 255).astype("uint8"), mode="L")
+    #             mask = mask.resize(img.size, Image.LANCZOS)
+    #             masks.append(mask)
+    #     return masks
+
     def predict(self, img: PILImage, *args, **kwargs) -> List[PILImage]:
-        """
-        Predict the mask of the input image.
-
-        This method takes an image as input, preprocesses it, and performs a prediction to generate a mask. 
-        The generated mask is then post-processed and returned as a list of PILImage objects.
-
-        Parameters:
-            img (PILImage): The input image to be processed.
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-
-        Returns:
-            List[PILImage]: A list of post-processed masks.
-        """
+        """简化版, 只需要第一张 mask"""
         normed_img = self.normalize(img, (0.5,0.5,0.5), (1.0, 1.0, 1.0), (768, 768)) # 可以上 1024x1024
 
         with torch.no_grad():
             masks_pt = self.model(torch.tensor(normed_img, device=device))
         
         masks = []
-        for i in range(2):
-            for j in range(2):
-                mask = masks_pt[i][j][0, 0].cpu().numpy()
-                ma, mi = np.max(mask), np.min(mask)
-                mask = (mask - mi) / (ma - mi)
-                mask = Image.fromarray((mask * 255).astype("uint8"), mode="L")
-                mask = mask.resize(img.size, Image.LANCZOS)
-                masks.append(mask)
+        mask = masks_pt[0][0][0, 0].cpu().numpy()
+        ma, mi = np.max(mask), np.min(mask)
+        mask = (mask - mi) / (ma - mi)
+        mask = Image.fromarray((mask * 255).astype("uint8"), mode="L")
+        mask = mask.resize(img.size, Image.LANCZOS)
+        masks.append(mask)
         return masks
+    
+    def predict_batch(self, imgs: List[PILImage], *args, **kwargs) -> List[List[PILImage]]:
+        """简化版"""
+        batch_size = 16
+        batches = batch_list(imgs, batch_size)  # tiny batches
+        results = []
+        for batch in batches:
+            normed_batch = self.normalize_batch(batch)
+            normed_batch = torch.tensor(normed_batch, device=device)
+            with torch.no_grad():
+                masks = self.model(normed_batch)[0][0].squeeze(1).cpu().numpy() # (batch, h, w)
+            ma, mi = np.max(masks), np.min(masks)
+            masks = (masks - mi) / (ma - mi)
+            for i in range(masks.shape[0]):
+                mask = Image.fromarray((masks[i] * 255).astype("uint8"), mode="L")
+                results.append(mask)
+        for i, mask in enumerate(results):
+            results[i] = mask.resize(imgs[i].size, Image.LANCZOS)
+        return results
 
     def normalize(
         self,
         img: PILImage,
-        mean: Tuple[float, float, float],
-        std: Tuple[float, float, float],
-        size: Tuple[int, int],
+        mean: Tuple[float, float, float] = (0.5,0.5,0.5),
+        std: Tuple[float, float, float] = (1.0, 1.0, 1.0),
+        size: Tuple[int, int] = (768, 768),
         *args,
         **kwargs
     ) -> np.ndarray:
         im = img.convert("RGB").resize(size, Image.LANCZOS)
-
         im_ary = np.array(im)
-        im_ary = im_ary / np.max(im_ary)
-
-        tmpImg = np.zeros((im_ary.shape[0], im_ary.shape[1], 3))
+        im_ary = im_ary / im_ary.max()
+        tmpImg = np.zeros(im_ary.shape)
         tmpImg[:, :, 0] = (im_ary[:, :, 0] - mean[0]) / std[0]
         tmpImg[:, :, 1] = (im_ary[:, :, 1] - mean[1]) / std[1]
         tmpImg[:, :, 2] = (im_ary[:, :, 2] - mean[2]) / std[2]
-
         tmpImg = tmpImg.transpose((2, 0, 1))
-
         return np.expand_dims(tmpImg, 0).astype(np.float32)
+
+    def normalize_batch(
+        self,
+        imgs: List[PILImage],
+        mean: Tuple[float, float, float] = (0.5,0.5,0.5),
+        std: Tuple[float, float, float] = (1.0, 1.0, 1.0),
+        size: Tuple[int, int] = (768, 768),
+        *args,
+        **kwargs
+    ) -> np.ndarray:
+        rbgs = [img.convert("RGB").resize(size, Image.LANCZOS) for img in imgs]
+        rbgs_array = np.array(rbgs)
+        rbgs_array = rbgs_array / rbgs_array.max()
+        tmpImgs = np.zeros(rbgs_array.shape)
+        tmpImgs[:, :, :, 0] = (rbgs_array[:, :, :, 0] - mean[0]) / std[0]
+        tmpImgs[:, :, :, 1] = (rbgs_array[:, :, :, 1] - mean[1]) / std[1]
+        tmpImgs[:, :, :, 2] = (rbgs_array[:, :, :, 2] - mean[2]) / std[2]
+        tmpImgs = tmpImgs.transpose((0, 3, 1, 2))
+        return tmpImgs.astype(np.float32)
 
     @classmethod
     def download_models(cls, *args, **kwargs):
--- webui.py
+++ webui.py
@@ -25,9 +25,7 @@ model_options = ['silueta', 'isnet-general-use', 'isnet-anime', 'u2net', 'u2netp
 model_dict = {name: None for name in model_options}
 # TODO: 如果某些模型使用频率不高，主动释放对应的内存
 
-def get_masked_img(model, img:PILImage) -> PILImage:
-    """图像蒙版预测，输出图片通道为 RGBA """
-    mask = model.predict(img)[0]  # 模型预测蒙版，图像大小缩放的逻辑已内置
+def put_alpha(img, mask):
     if img.mode != 'RGBA':
         img = img.convert('RGBA')
     if mask.mode != 'L':
@@ -35,25 +33,28 @@ def get_masked_img(model, img:PILImage) -> PILImage:
     img.putalpha(mask)
     return img
 
-def handle_up_image(pil_img, model_str="silueta"):
-    """处理上传文件的逻辑，参数个数和返回的长度对应 gradio 中绑定的 inputs 和 outputs"""
-    if not pil_img:
-        return None
-    print('using: ', model_str)
-
+def get_model(model_str):
     if model_str not in model_options:
-        return None
-    
+        raise ValueError("model not supported")
+    if not model_dict[model_str]:
+        model_dict[model_str] = new_session(model_str)
+    return model_dict[model_str]
+
+def handle_single_upload(pil_img, model_str="silueta"):
+    """处理上传文件的逻辑，参数个数和返回的长度对应 gradio 中绑定的 inputs 和 outputs"""
     try:
-        t1 = time.time()
-        if not model_dict[model_str]:
-            model_dict[model_str] = new_session(model_str)
-        masked_image = get_masked_img(model_dict[model_str], pil_img)
-        t2 = time.time()
-        print(f'delta time: {t2-t1}')
+        if not pil_img:
+            raise ValueError("")
+        start_time = time.time()
+        """图像蒙版预测，输出图片通道为 RGBA """
+        mask = get_model(model_str).predict(pil_img)[0]  # 模型预测蒙版，图像大小缩放的逻辑已内置
+        masked_image = put_alpha(pil_img, mask)
+        dt = time.time() - start_time
+        print(f'model: {model_str}, dt: {dt:.3f}s')
         return masked_image
     except Exception as e:
-        print(e)
+        if len(str(e))>0:
+            print(f"model: {model_str}, err: {e}")
         return None
 
 def base_png(image_dir):
@@ -61,39 +62,49 @@ def base_png(image_dir):
     base = os.path.basename(image_dir).split('.')[0]
     return base + ".png"
 
-def batch_infer(imgs, model_str="silueta"):
-    pass
-
-def multiple_files(files, tmp):
+def handle_multiple_upload(files, model_str="silueta"):
     # All files are images
     if not files:
         return None
     
-    bytesIO_images = []
+    named_images = {}
     for f in files:
         # Open the image and handle it
         pil_img = Image.open(f.name)
-        out_img = handle_up_image(pil_img)
+        named_images[base_png(f.name)] = pil_img
 
-        # 将处理结果保存在内存中
-        img_bytes = io.BytesIO()
-        out_img.save(img_bytes, format='PNG')
+    #===================== infer using model ============#
+    start_time = time.time()
+    if model_str == 'rmbg14': # batch torch infer
+        masked_imgs = get_model(model_str).predict_batch(list(named_images.values()))
+        for i, (k, img) in enumerate(named_images.items()):
+            named_images[k] = put_alpha(img, masked_imgs[i])
+            # TODO: put_alpha 这个操作在前端完成，性能提升空间很大，网络传输的数据量变为 1/4
+    else:
+        model = get_model(model_str)
+        for name, pil_img in named_images.items():
+            mask = model.predict(pil_img)[0]
+            masked_image = put_alpha(pil_img, mask)
+             # 将处理结果保存在内存中 bytesiio or pils
+            named_images[name] = masked_image
+    dt = time.time() - start_time
+    print(f'model: {model_str}, dt: {dt:.3f}s')
+    #=====================================================#
 
-        # Append the image
-        bytesIO_images.append((base_png(f.name), img_bytes))
-        
     # zip文件命名为 {begin}-{end}.zip
     folder = os.path.dirname(files[0].name)
     begin = os.path.basename(files[0].name).split('.')[0][:7]
     end = os.path.basename(files[-1].name).split('.')[0][:7]
     zip_path = os.path.join(folder, f'{begin}-{end}.zip')
-    
+
     # 写入zip文件
     with zipfile.ZipFile(zip_path, 'w') as myzip:
-        for name, bimg in bytesIO_images:
-            # Write BytesIO object to zip file
-            bimg.seek(0) # go to the start of the BytesIO object
-            myzip.writestr(name, bimg.read()) 
+        for name, pil_img in named_images.items():
+            if pil_img is None: continue
+            img_bytes = io.BytesIO()
+            pil_img.save(img_bytes, format='PNG')  # Write BytesIO object to zip file
+            img_bytes.seek(0)                      # Go to the start of the BytesIO object
+            myzip.writestr(name, img_bytes.read())
 
     # 临时文件不用保存
     for f in files:
@@ -116,16 +127,16 @@ with gr.Blocks(css=css) as app:
             select_model = gr.Radio(label="抠图模型", choices=model_options, value=model_options[0])
             model_hint = gr.Markdown(value=model_hint)
             # retry_btn = gr.Button('回炉重抠')
-    origin_image.change(fn=handle_up_image, inputs=[origin_image, select_model], outputs=[result_image])
-    select_model.change(fn=handle_up_image, inputs=[origin_image, select_model], outputs=[result_image])
+    origin_image.change(fn=handle_single_upload, inputs=[origin_image, select_model], outputs=[result_image])
+    select_model.change(fn=handle_single_upload, inputs=[origin_image, select_model], outputs=[result_image])
 
     with gr.Tab(label='批处理'):
         with gr.Row():
             files_in = gr.File(label="拖入多个文件", show_label=True, file_count='multiple', file_types=['image'], type='filepath', interactive=True)
             files_out = gr.File(label='输出', visible=True)
         with gr.Row():
-            select_model2 = gr.Radio(label="抠图模型", choices=[model_options[0]], value=model_options[0])
-    files_in.change(fn=multiple_files, inputs=[files_in, select_model2], outputs=[files_out])
+            select_model2 = gr.Radio(label="抠图模型", choices=model_options, value=model_options[0])
+    files_in.change(fn=handle_multiple_upload, inputs=[files_in, select_model2], outputs=[files_out])
 
 app.launch(
     server_name="0.0.0.0",
